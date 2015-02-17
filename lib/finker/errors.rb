bd0d86a6541cafc200125718aeab29802c3df4f1
long-long-float <niinikazuki@yahoo.co.jp>
2015-02-18 00:55:23 +0900

    ConfigFileNotFoundエラーにメッセージを追加した

--- lib/finker/errors.rb
+++ lib/finker/errors.rb
@@ -3,7 +3,11 @@ module Finker
     class Error < StandardError; end
 
     class FileNotFound < Error; end
-    class ConfigFileNotFound < Error; end
+    class ConfigFileNotFound < Error
+      def message
+        "could not find config file(#{Finker::CONFIG_FILE})"
+      end
+    end
 
   end
 end
